fe03cf26d6676b9a1f990b1ee2e3e81e0c7e2d92
Jc Canlas <johnrolandchristopher@gmail.com>
2026-09-22 18:43:31 +0800

    Added dbt project files.

---
+++ dbt/macros/generate_schema_name.sql
@@ -0,0 +1,8 @@
+{# Keep configured schema names such as silver and mart instead of adding the target schema prefix. #}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+  {%- if custom_schema_name is none -%}
+    {{ target.schema }}
+  {%- else -%}
+    {{ custom_schema_name | trim }}
+  {%- endif -%}
+{%- endmacro %}
